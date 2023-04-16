2241914d2726af122b6352d923c028381df406a3
Dominik Młynarczyk <dominikmlynarczyk@gmail.com>
2023-04-16 21:19:40 +0200

    feat(e2e): Add sample (naive) solution for e2e todo app testing

--- testing/e2e/cypress/e2e/TodoModule/TodoModule.cy.ts
+++ testing/e2e/cypress/e2e/TodoModule/TodoModule.cy.ts
@@ -3,14 +3,53 @@ describe('Todo Module tests', () => {
     cy.visit('http://localhost:3000')
   })
 
-  describe('Adding todo tests', () => {
-    it('should assert if add button is disabled by default', () => {
-      const addTodoButton = cy.get('#add-todo-button')
-      addTodoButton.should('css')
+  describe('Todo', () => {
+    it('should properly follow Todo CRUD flow', () => {
+      cy.get('#todo-input').should('be.visible').should('have.value', '')
+      cy.contains(/^ADD NEW TODO$/)
+        .should('be.visible')
+        .should('be.disabled')
 
-      cy.get('#todo-input').type('costam')
+      const todoContent = 'Sample todo'
+      cy.get('#todo-input').type(todoContent)
+      cy.contains(/^ADD NEW TODO$/)
+        .should('not.be.disabled')
+        .click()
 
-      addTodoButton.click()
+      cy.contains(todoContent)
+        .should('be.visible')
+        .click()
+        .should(
+          'have.css',
+          'text-decoration',
+          'line-through solid rgb(0, 0, 0)',
+        )
+
+      cy.get('.sc-hLBbgP').click()
+      cy.contains(todoContent).should('not.exist')
+
+      cy.get('#todo-input').type(todoContent)
+      cy.contains(/^ADD NEW TODO$/)
+        .should('not.be.disabled')
+        .click()
+
+      cy.contains(/^EDIT$/).click()
+      cy.get(`input[value="${todoContent}"]`)
+      cy.contains(/^EDIT$/).should('not.exist')
+      cy.get('.sc-hLBbgP').should('not.exist')
+      cy.contains(/^SAVE$/).should('exist')
+
+      cy.get(`input[value="${todoContent}"]`).clear()
+      cy.contains(/^SAVE$/).should('be.disabled')
+
+      const newTodoContent = 'Sample content 2'
+      cy.get(`input[value="${todoContent}"]`).type(newTodoContent)
+      cy.contains(/^SAVE$/)
+        .should('not.be.disabled')
+        .click()
+      cy.contains(newTodoContent).should('exist')
+      cy.contains(/^EDIT$/).should('exist')
+      cy.get('.sc-hLBbgP').should('exist')
     })
   })
 })
